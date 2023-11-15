f8c38f49c48e57f307e17bef0ae07f9c7a7cec26
MBSoo <qud9783@khu.ac.kr>
2023-11-15 15:22:27 -0500

    tab setting and font setting

--- App.js
+++ App.js
@@ -5,13 +5,95 @@ import SignInScreen from './screens/SignInScreen';
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Profile from './screens/profile/profile';
 import Recommendation from './screens/Recommendation/Recommendation';
+import * as React from "react";
+import * as Font from "expo-font";
+// import mainFont from "./assets/Fonts";
 
+function HomeScreen() {
+  return (
+    <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
+      <Text> Just Testing... This is Home! </Text>
+    </View>
+  )
+}
+
+function StatScreen() {
+  return (
+    <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
+      <MainFont style={{fontFamily:"font-ExtraBold", fontSize: 60}}> Just Testing... This is Home! </MainFont>
+    </View>
+  )
+}
+
+function RecommendScreen() {
+  return (
+    <Recommendation/>
+  )
+}
+
+function ProfileScreen() {
+  return (
+    <Profile />
+  )
+}
+
+const MainFont = (props) => {
+  const [fontLoaded, setFontLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        "font-Regular": require('./assets/Fonts/Pretendard-Regular.otf'),
+        "font-Medium": require('./assets/Fonts/Pretendard-Medium.otf'),
+        "font-Light": require('./assets/Fonts/Pretendard-Light.otf'),
+        "font-Bold": require('./assets/Fonts/Pretendard-Bold.otf'),
+        "font-SemiBold": require('./assets/Fonts/Pretendard-SemiBold.otf'),
+        "font-ExtraBold": require('./assets/Fonts/Pretendard-ExtraBold.otf'),
+      });
+        setFontLoaded(true);
+        console.log("Success!");
+    }
+    loadFont();
+  }, []);
+  if (!fontLoaded) {
+    return <Text>Loading... </Text>;
+  }
+  return (
+    <Text style={{fontFamily: "font-ExtraBold"}}>{props.children}</Text>
+  )
+}
 
 export default function App() {
-  const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
+  React.useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        "font-Regular": require('./assets/Fonts/Pretendard-Regular.otf'),
+        "font-Medium": require('./assets/Fonts/Pretendard-Medium.otf'),
+        "font-Light": require('./assets/Fonts/Pretendard-Light.otf'),
+        "font-Bold": require('./assets/Fonts/Pretendard-Bold.otf'),
+        "font-SemiBold": require('./assets/Fonts/Pretendard-SemiBold.otf'),
+        "font-ExtraBold": require('./assets/Fonts/Pretendard-ExtraBold.otf'),
+      });
+        setFontLoaded(true);
+        console.log("Success!");
+        Text.fontFamily = "font-ExtraBold";
+        
+    }
+    loadFont();
+  }, []);
+
   return (
-    <Recommendation />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={{headerShown: false}}></Tab.Screen>
+        <Tab.Screen name="Statisitics" component={StatScreen} options={{headerShown: false}}></Tab.Screen>
+        <Tab.Screen name="Recommend" component={RecommendScreen} options={{headerShown: false}}></Tab.Screen>
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{headerShown: false}}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
