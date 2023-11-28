7448082a39ab7b0dc3b9e4ee5980f5c0d7aa70d9
MBSoo <qud9783@khu.ac.kr>
2023-11-28 10:30:17 -0500

    Statistics 네비게이션 스택 추가

--- screens/Main/main.js
+++ screens/Main/main.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "react-native-gesture-handler";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 import Profile from "../profile/profile"
 import Recommendation from '../Recommendation/Recommendation';
-import Statisitics from '../Statistics/Statistics';
+import Statistics from '../Statistics/Statistics';
+import DailyStatistics from '../Statistics/DailyStatistics';
 import * as React from "react";
 import * as Font from "expo-font";
+import SessionStatistics from '../Statistics/SessionStatistics';
 
 
 function HomeScreen() {
@@ -19,7 +22,7 @@ function HomeScreen() {
   
   function StatScreen() {
     return (
-      <Statisitics />
+      <Statistics />
     )
   }
   
@@ -34,12 +37,29 @@ function HomeScreen() {
       <Profile />
     )
   }
-export default function Main({navigation}) {
+  function StatNavigator() {
+    const StatStack = createStackNavigator();
+    return (
+      <StatStack.Navigator>
+        <StatStack.Screen name="Statistics" component={Statistics} options={{headerShown: false}} />
+        <StatStack.Screen name='DailyStatistics' component={DailyStatistics} options={({route}) => ({
+          title: `${route.params.id}`,
+          headerBackTitleVisible: false,
+        })} />
+        <StatStack.Screen name="SessionStatistics" component={SessionStatistics} options={({route}) => ({
+          title: `Session ${route.params.id}`,
+          headerBackTitleVisible: false,
+        })} />
+      </StatStack.Navigator>
+    )
+  }
+export default function Main() {
     const Tab = createBottomTabNavigator();
+    
     return (
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} options={{headerShown: false}}></Tab.Screen>
-        <Tab.Screen name="Statisitics" component={StatScreen} options={{headerShown: false}}></Tab.Screen>
+        <Tab.Screen name="Statistics" component={StatNavigator} options={{headerShown: false}}></Tab.Screen>
         <Tab.Screen name="Recommend" component={RecommendScreen} options={{headerShown: false}}></Tab.Screen>
         <Tab.Screen name="Profile" component={ProfileScreen} options={{headerShown: false}}></Tab.Screen>
       </Tab.Navigator>
