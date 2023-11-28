8a2a67d6bf49a3afe27698a707d014d3567a92f3
MBSoo <qud9783@khu.ac.kr>
2023-11-28 10:31:26 -0500

    Component 추가 Circular Progress 생성

---
+++ Components/CircularProgress.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, } from 'react-native';
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import Svg, {Circle} from 'react-native-svg';
+import Donut from './CircleProgress';
+
+const data = [{
+  percentage: 50,
+  color: "#5B30E6",
+  max: 100
+}]
+
+
+
+export default function CircularProgress() {
+      return (
+        <View style={styles.container}>
+          <StatusBar hidden/>
+          <View style={{flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap', alignItems: 'center'}}>
+            {data.map((p, i) => {
+              return <Donut key={i} percentage={p.percentage} color={p.color} delay={500 + 100 * i} max={p.max} radius={60}/>
+            })}
+          </View>
+        </View>
+      );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 0,
+    backgroundColor: '#fff',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
